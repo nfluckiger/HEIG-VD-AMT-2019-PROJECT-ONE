824a58b95d999463c6d5cdf3e39e278007e9c0e9
David Jaquet <djaquet5@gmail.com>
2019-11-06 10:37:37 +0100

    Add empty servlet

---
+++ src/ch/heigvd/amt/servlet/Login.java
@@ -0,0 +1,8 @@
+package ch.heigvd.amt.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+}
