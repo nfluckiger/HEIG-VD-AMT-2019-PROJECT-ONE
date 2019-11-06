96ac1603b6ab2def507fcc4915d522f459bb2bd1
Saylox <nathan.fluckiger@hotmail.ch>
2019-11-06 11:10:03 +0100

    WIP Model test files¨

---
+++ src/ch/heigvd/amt/test/app/model/TestGame.java
@@ -0,0 +1,18 @@
+package ch.heigvd.amt.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class GameTest {
+
+    @Test
+    public void itShouldBePossibleToCreateAGame() {
+
+        assertEquals("oliechti", note.getAuthor().getUsername());
+        assertEquals("hello world", note.getContent());
+        assertEquals(99, note.getId());
+        assertFalse(note.isDeleted());
+    }
+}
